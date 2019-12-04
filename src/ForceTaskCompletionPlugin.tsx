8a48ec516ec25f50682d65607756c8d1f2edba3e
Alec Ortega <aleccortega@gmail.com>
2019-12-04 12:52:06 -0500

    Remove ezCater feature flag library

--- src/ForceTaskCompletionPlugin.tsx
+++ src/ForceTaskCompletionPlugin.tsx
@@ -4,9 +4,6 @@ import registerReducers from 'registerReducers';
 import registerListeners from 'registerListeners';
 import registerNotifications from 'registerNotifications';
 import showNotificationIfTaskExists from 'utilities/showNotificationIfTaskExists';
-import { createFeatureFlagsClient } from '@ezcater/feature-flags-js';
-
-const featureFlagsClient = createFeatureFlagsClient();
 
 declare global {
   interface Window {
@@ -15,7 +12,6 @@ declare global {
 }
 
 const PLUGIN_NAME = 'ForceTaskCompletionPlugin';
-const FEATURE_FLAG_KEY = 'Liberty:PluginForceTaskCompletion';
 
 class ForceTaskCompletionPlugin extends FlexPlugin {
   constructor() {
@@ -25,21 +21,11 @@ class ForceTaskCompletionPlugin extends FlexPlugin {
   // This code is run when your plugin is being started
   // Use this to modify any UI components or attach to the actions framework
   init(flex: typeof Flex, manager: Flex.Manager) {
-    featureFlagsClient
-      .get({
-        featureFlags: [FEATURE_FLAG_KEY],
-        trackingId: manager.workerClient.attributes.trackingId,
-        whitelistValues: [manager.workerClient.attributes.email],
-      })
-      .then(featureFlags => {
-        if (featureFlags[FEATURE_FLAG_KEY]) {
-          registerReducers(manager);
-          registerListeners(manager);
-          registerNotifications();
+    registerReducers(manager);
+    registerListeners(manager);
+    registerNotifications();
 
-          showNotificationIfTaskExists();
-        }
-      });
+    showNotificationIfTaskExists();
   }
 }
 
